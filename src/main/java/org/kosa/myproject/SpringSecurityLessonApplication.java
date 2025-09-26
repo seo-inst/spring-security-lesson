cbe87f35a09abd7d61e5f2e92c5d6b5506f1a704
seo-inst <innerpeace.deepi@gmail.com>
2025-09-26 09:40:23 +0900

    Initial commit

---
+++ src/main/java/org/kosa/myproject/SpringSecurityLessonApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLessonApplication.class, args);
+	}
+
+}
